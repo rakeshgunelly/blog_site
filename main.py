f98ad0f7d2d07b57fedbb8419eb2c5e03aa8548e
Rakesh Gunelly <rakeshgunelly@gmail.com>
2023-01-29 17:35:07 +0530

    blog site database creation

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from database import models
+from database.database import engine
+
 
 app = FastAPI()
 
 
 @app.get('/')
-def helloworld():
+def hello_world():
     return 'Hello World'
+
+
+models.Base.metadata.create_all(engine)
